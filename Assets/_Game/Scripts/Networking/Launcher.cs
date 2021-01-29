d420684ff59e494588683befcefe478b643be8f7
Utfrugg <170462@ade-nhtv.nl>
2021-01-29 14:30:11 +0100

    Added TMPro, Added Launcher Scene and basic connecting

---
+++ Assets/_Game/Scripts/Networking/Launcher.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Store these settings in a separate class or struct perhaps
+
+    private string roomName = "EpicRoom";
+    private string gameVersion = "1";
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void ConnectToPhoton()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void JoinRoom()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            Debug.Log("Connected! Creating/Joining room " + roomName);
+
+            //Here is where max players and such can be passed along
+            RoomOptions roomOptions = new RoomOptions();
+            TypedLobby typedLobby = new TypedLobby(roomName, LobbyType.Default);
+            PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby);
+        }
+    }
+
+    public void LoadArena()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount > 1)
+        {
+            PhotonNetwork.LoadLevel("MainArena");
+        }
+        else
+        {
+            
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        JoinRoom();
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        string players = "";
+        foreach (var player in PhotonNetwork.CurrentRoom.Players)
+        {
+            players += player.Value.NickName + " ";
+        }
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + ": " + players);
+    }
+}
---
+++ Assets/_Game/Scripts/Networking/PlayerNameInputField.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class PlayerNameInputField : MonoBehaviour
+{
+    private static string playerNamePrefKey = "PlayerName";
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string defaultName = "";
+        TMP_InputField inputField = GetComponent<TMP_InputField>();
+
+        if (PlayerPrefs.HasKey(playerNamePrefKey))
+        {
+            defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+            inputField.text = defaultName;
+        }
+
+        PhotonNetwork.LocalPlayer.NickName = defaultName;
+    }
+
+    public void SetPlayerName(string pname)
+    {
+        PhotonNetwork.LocalPlayer.NickName = pname + " ";
+        PlayerPrefs.SetString(playerNamePrefKey, pname);
+    }
+}
